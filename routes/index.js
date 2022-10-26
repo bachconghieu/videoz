cffb17e997b89e4737cf50b3c9397e05ccaa7adf
Bach Van Hieu <91191584+bachconghieu@users.noreply.github.com>
2022-10-26 12:55:46 +0700

    ff

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('Code', { title: 'Express' });
+});
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
